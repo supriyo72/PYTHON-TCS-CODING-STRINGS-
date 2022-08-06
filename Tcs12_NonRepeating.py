73a8ca70034cb13c0a51bbccfe9f65b61601d745
Supriyo Shankar Ghosh <89015232+supriyo72@users.noreply.github.com>
2022-08-07 02:14:30 +0530

    Update Tcs12_NonRepeating.py

--- Tcs12_NonRepeating.py
+++ Tcs12_NonRepeating.py
@@ -1,16 +1,3 @@
-# alpha= str(input("Enter the string: "))
-
-# count=0
-# for i in range(0,len(alpha)):
-#     for j in range(0,len(alpha)):
-#         if alpha[i] == alpha[j]:
-#             count=count+1
-            
-
-
-
-
-
 alpha= str(input("Enter the string: "))
 alpha= alpha.lower()
 for i in alpha:
